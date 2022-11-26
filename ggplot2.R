7578eb5406faa8a27cd25d2cecee2a5beae8dbb9
Alexandre Yudi <61790578+AlexYud@users.noreply.github.com>
2022-11-26 19:14:50 -0300

    Create ggplot2.R

---
+++ ggplot2.R
@@ -0,0 +1,30 @@
+# Referência utilizada: https://ggplot2.tidyverse.org/reference/
+
+library(tidyverse)
+library(cowplot)
+
+View(SINASC_2020_compilado)
+
+test <- municipios %>% inner_join(SINASC_2020_2, by = c("CODMUNIC" = "CODMUNNASC"))
+
+test %>% select(CODMUNIC, uf_code, IDADEMAE, CONSULTAS, SEXO, PESO, IDANOMAL, SEMAGESTAC, KOTELCHUCK)
+
+test %>% filter(IDADEMAE < 18) %>% group_by(MUNICIPIO) %>% count(sort = TRUE, uf)
+
+hist(test)
+
+ggplot(test, aes(x=test$n)) + geom_dotplot()
+
+ggplot(test, aes(x=test$n)) + geom_map()
+
+test2 <- test %>% select(CODMUNNASC) %>% group_by(CODMUNNASC) %>% count()
+
+boxplot(test2)
+
+test %>% filter(PESO > 5000) %>% group_by(MUNICIPIO) %>% count(sort = TRUE, uf)
+
+boxplot(test)
+
+ggplot(test, aes(x=test$n)) + geom_bar()
+
+ggplot(test, aes(x=test$n)) + geom_density()
