4c26ef16325c8c39680b1bfb138b239b9c6a3c37
Alexandre Yudi <61790578+AlexYud@users.noreply.github.com>
2022-11-26 18:14:50 -0300

    Update comparando_shoppings.r

--- Aula 1/comparando_shoppings.r
+++ Aula 1/comparando_shoppings.r
@@ -1,3 +1,5 @@
+# Referência utilizada: https://stackoverflow.com/questions/1249548/side-by-side-plots-with-ggplot2
+
 library(tidyverse)
 library(cowplot)
 
@@ -9,7 +11,6 @@ shop1 <- ggplot(shopping, aes(x=shopping$chaveiro)) +
 shop2 <- ggplot(shopping2, aes(x=shopping2$chaveiro)) + 
   geom_bar()
 
-plot_grid(shop1, shop2, labels = "AUTO")
+plot_grid(shop1, shop2, labels = "AUTO") # visualização
 
-# Resultado: O shopping 1 possui até 6 áreas com 3 chaveiros enquanto o shopping 2 mais de 10 áreas com apenas 1 chaveiro
-
+# Resultado: O shopping 1 possui até 6 áreas com 3 chaveiros enquanto o shopping 2 tem mais de 10 áreas com apenas 1 chaveiro
