79a0ef8e94e3248911ff8c7745a701376cd76913
Alexandre Yudi <61790578+AlexYud@users.noreply.github.com>
2022-11-26 19:09:04 -0300

    Create manipular_dados_dplyr.R

---
+++ manipular_dados_dplyr.R
@@ -0,0 +1,21 @@
+# Referência utilizada: http://leg.ufpr.br/~walmes/cursoR/data-vis/slides/05-dplyr.pdf
+
+library(tidyverse)
+library(cowplot)
+
+View(SINASC_2020_compilado)
+
+test <- municipios %>% inner_join(SINASC_2020_2, by = c("CODMUNIC" = "CODMUNNASC"))
+
+test %>% select(CODMUNIC, uf_code, IDADEMAE, CONSULTAS, SEXO, PESO, IDANOMAL, SEMAGESTAC, KOTELCHUCK)
+
+test %>% filter(IDADEMAE < 18) %>% group_by(MUNICIPIO) %>% count(sort = TRUE, uf)
+
+hist(test)
+
+
+test %>% filter(PESO > 5000) %>% group_by(MUNICIPIO) %>% count(sort = TRUE, uf)
+
+boxplot(test)
+
+ggplot(test, aes(x=test$n)) + geom_bar()
