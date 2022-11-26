804b15ed6776ad6652bcbc1132115fc0977b073c
Alexandre Yudi <61790578+AlexYud@users.noreply.github.com>
2022-11-26 18:46:32 -0300

    Create shinydashboard.R

---
+++ shinydashboard.R
@@ -0,0 +1,21 @@
+ui.R
+install.packages("shinydashboard")
+library(shinydashboard)
+
+
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+
+app
+
+
+library(shinydashboard)
+
+
+
+
+shinyApp(ui, server)
